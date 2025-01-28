0068b7fb4ad019731c1c5339d4c184f51cc7ec58
Andrew Mao <maosteram@gmail.com>
2025-01-28 13:23:26 -0600

    Fixes header padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,6 @@ import SocialLinks from "./util/SocialLinks";
 const Header = () => {
   return (
     <div className="header-content">
-      {/* <img src="https://placehold.co/200x200" alt="" className="header-image" /> */}
       <div className="header-intro">
         <h1 className="header-title">Howdy! I'm Andrew</h1>
         <p>
